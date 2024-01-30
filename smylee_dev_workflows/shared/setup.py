588319bff6b10e636810df3fd826a85728bef076
Patty R. <smyleeface@users.noreply.github.com>
2024-01-29 22:11:04 -0800

    chore: add setup.py package for shared modules

---
+++ smylee_dev_workflows/shared/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smylee-dev-workflows-shared',
+    version='0.1.0',
+    packages=find_packages(),
+    author='PattyR',
+    author_email='patty.ramert@gmail.com',
+    description='Shared modules in Dev Workflow',
+    long_description='Shared modules for processing events and notifications.',
+    url='https://github.com/smyleeface/smylee-dev-workflows/',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
