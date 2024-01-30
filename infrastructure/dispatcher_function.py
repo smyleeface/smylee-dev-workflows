074ab027d5a075afe7c8eaac6a2911e1b6bcb1f8
Patty R. <smyleeface@users.noreply.github.com>
2024-01-29 16:02:30 -0800

    feat: add kms and parameter store permissions

--- infrastructure/dispatcher_function.py
+++ infrastructure/dispatcher_function.py
@@ -11,10 +11,13 @@ from awacs.aws import Action, Allow, PolicyDocument, Principal, Statement
 
 class DispatcherFunction:
 
-    def __init__(self, function_name, application_zip, api_gateway_rest_api):
+    def __init__(self, function_name, application_s3_param, application_zip_param, api_gateway_rest_api, app_parameter_store_path, primary_kms_arn):
         self._function_name = function_name
-        self._application_zip = application_zip
+        self._application_s3_param = application_s3_param
+        self._application_zip_param = application_zip_param
         self._api_gateway_rest_api = api_gateway_rest_api
+        self._app_parameter_store_path = app_parameter_store_path
+        self._primary_kms_arn = primary_kms_arn
         self.api_path_part = 'dispatcher'
 
     def get_function_definition(self, function_role) -> awslambda.Function:
@@ -26,8 +29,9 @@ class DispatcherFunction:
             Runtime="python3.11",
             Timeout=60,
             Code=awslambda.Code(
-                ZipFile=self._application_zip
-            )
+                S3Bucket=Ref(self._application_s3_param),
+                S3Key=Ref(self._application_zip_param)
+            ),
         )
 
     def get_function_api_gateway_resource(self) -> apigateway.Resource:
@@ -53,6 +57,7 @@ class DispatcherFunction:
     def add_resource(self, current_template: Template) -> Template:
         function_role = self.get_function_role_and_policy()
         function_definition = self.get_function_definition(function_role)
+        function_event_invoke_config = self.get_function_event_invoke_config(function_definition)
         awslambda_permission = self.get_lambda_trigger_permissions(function_definition)
         api_gateway_resource = self.get_function_api_gateway_resource()
         api_gateway_method = self.get_function_api_gateway_method(api_gateway_resource, function_definition)
@@ -60,6 +65,7 @@ class DispatcherFunction:
         current_template.resources['DevWorkflowApiGatewayDeployment'].resource['DependsOn'] = api_gateway_method.title
         current_template.add_resource(function_role)
         current_template.add_resource(function_definition)
+        current_template.add_resource(function_event_invoke_config)
         current_template.add_resource(awslambda_permission)
         current_template.add_resource(api_gateway_resource)
         current_template.add_resource(api_gateway_method)
@@ -75,8 +81,7 @@ class DispatcherFunction:
                                                   ApiGatewayDefinition=Ref(self._api_gateway_rest_api),
                                                   PathPart=self.api_path_part))
 
-    @staticmethod
-    def get_function_role_and_policy() -> iam.Role:
+    def get_function_role_and_policy(self) -> iam.Role:
         allow_write_to_log_statement = Statement(
             Effect=Allow,
             Action=[
@@ -87,17 +92,37 @@ class DispatcherFunction:
             Resource=["*"],
         )
 
+        allow_get_parameter_statement = Statement(
+            Effect=Allow,
+            Action=[
+                Action("ssm", "GetParameter*"),
+            ],
+            Resource=[Sub("arn:aws:ssm:${AWS::Region}:${AWS::AccountId}:parameter${AppParameterStorePath}/*",
+                          AppParameterStorePath=self._app_parameter_store_path)],
+        )
+
+        allow_kms_decrypt = Statement(
+            Effect=Allow,
+            Action=[
+                Action("kms", "Decrypt"),
+            ],
+            Resource=[self._primary_kms_arn],
+        )
+
         policy_document = PolicyDocument(
             Version="2012-10-17",
             Id="LambdaExecutionPolicy",
             Statement=[
-                allow_write_to_log_statement
+                allow_write_to_log_statement,
+                allow_get_parameter_statement,
+                allow_kms_decrypt
             ],
         )
 
+        # trust relationship for lambda
         assume_role_statement = Statement(
             Effect=Allow,
-            Principal=Principal("Service", ["lambda.amazonaws.com"]),
+            Principal=Principal("Service", ["lambda.amazonaws.com", "ssm.amazonaws.com"]),
             Action=[Action("sts", "AssumeRole")]
         )
 
@@ -122,6 +147,11 @@ class DispatcherFunction:
 
         return function_execution_role
 
-
-
-
+    @staticmethod
+    def get_function_event_invoke_config(function_definition: awslambda.Function) -> awslambda.EventInvokeConfig:
+        return awslambda.EventInvokeConfig(
+            "DevWorkflowEventInvokeConfig",
+            FunctionName=Ref(function_definition),
+            MaximumRetryAttempts=0,
+            Qualifier="$LATEST",
+        )
--- infrastructure/main.py
+++ infrastructure/main.py
@@ -1,16 +1,33 @@
 import os
 
-from troposphere import Template
+from troposphere import Template, Parameter, ImportValue
 from api_gateway import apis
 from dispatcher_function import DispatcherFunction
 
 import sys
 
-#todo create uuid for unique names that is used and stored in the cloudformation stack?
+
 if __name__ == "__main__":
     template_file = sys.argv[1]
 
     main_template = Template()
+
+    # parameters
+    primary_kms_arn = os.environ['KMS_KEY_ID_ARN']
+    github_app_private_key_ssm_path = Parameter(
+        "GHAppPrivateKeySSMPath",
+        Type="String",
+        Description="KMS Encrypted GitHub App Private Key SSM Parameter Path",
+        Default='/SmyleeDevWorkflows/GitHubApp/PrivateKey'
+    )
+
+    # primary s3 bucket for uploading artifacts
+    s3_bucket_for_artifacts_param_name = "BucketForUploadsUsWest2"
+    s3_buckets_for_artifacts_import = ImportValue("S3::BucketForUploadsUsWest2-Name")
+    s3_bucket_for_artifacts = Parameter(
+        s3_bucket_for_artifacts_param_name,
+        Type="String"
+    )
 
     # Base API Gateway
     api_gateway_rest_api, api_gateway_deployment, api_gateway_stage = apis.get_resource()
@@ -19,9 +36,22 @@ if __name__ == "__main__":
     main_template.add_resource(api_gateway_stage)
 
     # Dispatcher Function
-    dispatcher_function_package_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, "dispatcher_function_package.zip")
-    dispatcher_function = DispatcherFunction("DevWorkflow-DispatcherFunction", dispatcher_function_package_name, api_gateway_rest_api)
+    dispatcher_function_s3_zip_path_param_name = "DispatcherFunctionS3ZipPath"
+    dispatcher_function_s3_zip_path = Parameter(
+        dispatcher_function_s3_zip_path_param_name,
+        Type="String"
+    )
+    dispatcher_function = DispatcherFunction("DevWorkflow-DispatcherFunction",
+                                             s3_bucket_for_artifacts_param_name,
+                                             dispatcher_function_s3_zip_path_param_name,
+                                             api_gateway_rest_api,
+                                             '/SmyleeDevWorkflows',
+                                             primary_kms_arn)
     main_template = dispatcher_function.add_resource(main_template)
+
+    # add parameters to template
+    main_template.add_parameter(s3_bucket_for_artifacts)
+    main_template.add_parameter(dispatcher_function_s3_zip_path)
 
     with open(os.path.join(os.path.dirname(__file__), template_file), "w") as cf_file:
         cf_file.write(main_template.to_yaml())
