d13590abdc40c7d1eae676246bd0f768b6eebec0
Patty R. <smyleeface@users.noreply.github.com>
2024-02-09 21:03:15 -0800

    chore: refactor file structure

--- smylee_dev_workflows/functions/dispatcher/dispatch_event.py
+++
@@ -1,6 +0,0 @@
-def dispatch(payload, github_event_type):
-
-    action = payload['action']
-
-    if github_event_type == 'pull_request' and action in ['opened', 'reopened']:
-        print('pull request opened')
