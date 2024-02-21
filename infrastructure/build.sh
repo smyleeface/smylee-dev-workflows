4e413ae9fa920bdc8c44881bdfa38b7be705ae0a
Patty R. <smyleeface@users.noreply.github.com>
2024-02-21 14:53:48 -0800

    build: moved location of artifacts

--- infrastructure/build.sh
+++ infrastructure/build.sh
@@ -5,7 +5,8 @@ set -e
 source ../secrets/env_var
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
-BUILD_DATA_PATH=build_data
+BUILD_DATA_PATH=${REPO_ROOT}/infrastructure/build_data
+BUILD_ARTIFACTS=${REPO_ROOT}/infrastructure/build_artifacts
 UUID=$(uuidgen)
 BUILD_ID=${UUID##*-}
 SHA=$(git rev-parse --short $(git rev-parse HEAD))
@@ -13,39 +14,37 @@ REPO_NAME=$(basename $(git rev-parse --show-toplevel))
 S3_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name=='S3::BucketForUploadsUsWest2-Name'].Value" --output text)
 DISPATCHER_FUNCTION_ZIP_FILENAME="dispatcher_application-${SHA}-${BUILD_ID}.zip"
 
-echo "############ BUILD_ID: $BUILD_ID"
-echo "############ DISPATCHER_FUNCTION_ZIP_FILENAME: $DISPATCHER_FUNCTION_ZIP_FILENAME"
-
-cd ../functions/dispatcher
+echo "############ BUILD_ID: ${BUILD_ID}"
+echo "############ DISPATCHER_FUNCTION_ZIP_FILENAME: ${DISPATCHER_FUNCTION_ZIP_FILENAME}"
 
 # Ensure the build directory exists
-mkdir -p build
+mkdir -p build_artifacts/dispatcher
+
+cd ${REPO_ROOT}/functions/dispatcher/
 
 # Install the Python dependencies from requirements.txt into the build directory
-pip install --platform manylinux2014_x86_64 --only-binary=:all: --target build -r requirements.txt
+pip install --platform manylinux2014_x86_64 --only-binary=:all: --target ${BUILD_ARTIFACTS}/dispatcher -r requirements.txt
 
-# Copy application code to directory
-#cp -r handler.py build
-#cp -r dispatch_event.py build
-cp -r dev_workflow build
+## Copy application code to directory
+cp -r dev_workflow ${BUILD_ARTIFACTS}/dispatcher
 
-# Change to the build directory
-cd build
+## Change to the build directory
+cd ${BUILD_ARTIFACTS}/dispatcher
 
-# Create a ZIP file with the installed dependencies
-find . -type d -name "__pycache__" -prune -o -type f -print | zip -r ../../../$DISPATCHER_FUNCTION_ZIP_FILENAME -@
+## Create a ZIP file with the installed dependencies
+find . -type d -name "__pycache__" -prune -o -type f -print | zip -r ${BUILD_ARTIFACTS}/${DISPATCHER_FUNCTION_ZIP_FILENAME} -@
+#
+## Change back to the project's root
+cd ${REPO_ROOT}
+#
+## Upload the ZIP file to S3
+S3_FULL_PATH="$S3_BUCKET/$REPO_NAME/${DISPATCHER_FUNCTION_ZIP_FILENAME}"
+echo "############ Uploading ${DISPATCHER_FUNCTION_ZIP_FILENAME} to s3://$S3_FULL_PATH"
+aws s3 cp ${BUILD_ARTIFACTS}/${DISPATCHER_FUNCTION_ZIP_FILENAME} s3://$S3_FULL_PATH
 
-# Change back to the project's root
-cd ../../../infrastructure
-
-# Upload the ZIP file to S3
-S3_FULL_PATH="$S3_BUCKET/$REPO_NAME/$DISPATCHER_FUNCTION_ZIP_FILENAME"
-echo "############ Uploading $DISPATCHER_FUNCTION_ZIP_FILENAME to s3://$S3_FULL_PATH"
-aws s3 cp ../$DISPATCHER_FUNCTION_ZIP_FILENAME s3://$S3_FULL_PATH
-
-echo $BUILD_ID > $BUILD_DATA_PATH/build_id
-BUILD_ARTIFACTS="{ \"dispatcher_application\": \"$DISPATCHER_FUNCTION_ZIP_FILENAME\" }"
-echo $BUILD_ARTIFACTS | jq > $BUILD_DATA_PATH/build_artifacts
+echo ${BUILD_ID} > ${BUILD_DATA_PATH}/build_id
+BUILD_ARTIFACTS="{ \"dispatcher_application\": \"${DISPATCHER_FUNCTION_ZIP_FILENAME}\" }"
+echo ${BUILD_ARTIFACTS} | jq > ${BUILD_DATA_PATH}/build_manifest
 
 # # Clean up the build directory if needed
-# rm -r ../smylee_dev_workflows/functions/pull-request-open/build
+# rm -r ../smylee_dev_workflows/functions/pull_request__open/build
