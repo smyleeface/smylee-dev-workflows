7cc6e391d6af7b5eae4edf5f0fe86bd7c8b6fb71
Patty R <smyleeface@users.noreply.github.com>
2024-06-17 19:37:22 -0700

    Function and infrastructure for GitHub event, pull request merged (#6)
    * feat: initial files for pull request merged function
    
    * ci: add infrastructure and deploy pull request merged function
    
    * feat: add pull request merged event to dispatch events function

--- functions/dispatcher/dev_workflow/dispatcher/dispatch_event.py
+++ functions/dispatcher/dev_workflow/dispatcher/dispatch_event.py
@@ -3,10 +3,26 @@ import os
 
 def dispatch(sns_client, action, github_event_type, message):
 
-    if github_event_type == "pull_request" and action in ["opened", "reopened"]:
-        print("pull request opened")
-        sns_topic_arn = os.environ.get("PR__OPEN_SNS_TOPIC_ARN", "")
-        sns_client.publish(
-            TopicArn=sns_topic_arn,
-            Message=message,
-        )
+    if github_event_type == "pull_request":
+        if action in ["opened", "reopened"]:
+            print("pull request opened")
+            sns_topic_arn = os.environ.get("PR__OPEN_SNS_TOPIC_ARN", "")
+            try:
+                sns_client.publish(
+                    TopicArn=sns_topic_arn,
+                    Message=message,
+                )
+            except Exception as e:
+                print(f"Error: {e}")
+                exit(1)
+        elif action in ["merged"]:
+            print("pull request merged")
+            sns_topic_arn = os.environ.get("PR__MERGED_SNS_TOPIC_ARN", "")
+            try:
+                sns_client.publish(
+                    TopicArn=sns_topic_arn,
+                    Message=message,
+                )
+            except Exception as e:
+                print(f"Error: {e}")
+                exit(1)
