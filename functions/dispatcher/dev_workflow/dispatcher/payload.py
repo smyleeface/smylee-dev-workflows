b30daea53b6ed6a4f0905906f8292e4e28b8cd2a
Patty R <smyleeface@users.noreply.github.com>
2024-06-22 19:36:21 -0700

    add verify user functionality (#8)
    * chore: renamed files for build and deploy scripts
    
    * feat: added verified user check to dispatcher
    
    * feat: added user_access file to upload to s3
    
    * feat: output the bucket name the event data is stored
    
    * chore: refactor because it was erring printing the payload string
    
    * build: upload the user access list to app's the s3 bucket
    
    * feat: use the user access list from s3 to allow or deny access

--- functions/dispatcher/dev_workflow/dispatcher/payload.py
+++ functions/dispatcher/dev_workflow/dispatcher/payload.py
@@ -6,9 +6,12 @@ logger.setLevel(os.environ.get("LOGGING_LEVEL", logging.INFO))
 
 
 def upload(s3_client, bucket, key, payload):
-    logger.debug(s3_client, bucket, key, payload)
-    s3_client.put_object(
-        Bucket=bucket,
-        Key=key,
-        Body=payload.encode('utf-8')
-    )
+    try:
+        s3_client.put_object(
+            Bucket=bucket,
+            Key=key,
+            Body=payload.encode('utf-8')
+        )
+    except Exception as e:
+        logger.error(f"Error uploading payload to S3: {e}")
+        raise e
