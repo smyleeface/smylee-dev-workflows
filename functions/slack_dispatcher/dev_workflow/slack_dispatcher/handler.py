ce8daae128acb0a97e8434d6f5b2afbc8bbf683f
Patty R. <smyleeface@users.noreply.github.com>
2024-07-29 16:45:30 -0700

    feat: slash command prints message to slack channel

--- functions/slack_dispatcher/dev_workflow/slack_dispatcher/handler.py
+++ functions/slack_dispatcher/dev_workflow/slack_dispatcher/handler.py
@@ -1,6 +1,8 @@
 import json
 import logging
 import os
+import requests
+import urllib.parse
 
 import boto3
 from botocore.exceptions import ClientError
@@ -28,12 +30,47 @@ logger.setLevel(os.environ.get("LOGGING_LEVEL", logging.DEBUG))
 # payload_upload_bucket = os.environ.get("S3_BUCKET_FOR_PAYLOADS", "")
 
 
+def post_message(url, data):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response.status_code
+
+
+def parse_query_string(query_string):
+    parsed_query = urllib.parse.parse_qs(query_string)
+    parsed_query_single_values = {k: v[0] for k, v in parsed_query.items()}
+    return parsed_query_single_values
+
+
+def determine_action(requested_text):
+    action = requested_text.split(' ', 1)[0]
+    if action == "hugo-cover-image":
+        message = {
+            "text": "Generating cover image for Hugo please wait",
+            "response_type": "in_channel"
+        }
+    else:
+        message = {
+            "text": f"Unknown command `{action}`. Please try again.",
+            "response_type": "ephemeral"
+        }
+    return message
+
+
 def lambda_handler(event, context):
     logger.debug(event)
+    query_string = event.get("body", "")
+    parsed_json = parse_query_string(query_string)
+    print(json.dumps(parsed_json, indent=2))
+    requested_text = parsed_json.get("text", "")
+    message = determine_action(requested_text)
+    status_code = post_message(parsed_json.get("response_url", None), message)
 
     return {
         "isBase64Encoded": False,
-        "statusCode": 200,
+        "statusCode": status_code,
         "headers": {},
         "body": "completed",
     }
